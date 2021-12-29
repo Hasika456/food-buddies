701ff86cc5a9a7f09278ee44a953a6d8014be40b
Hasika456 <85883194+Hasika456@users.noreply.github.com>
2021-12-29 18:32:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function sweet(){
+    window.location="https://hasika456.github.io/sweet/";
+    
+}
+
+function spice(){
+    window.location="https://hasika456.github.io/spice/";
+    
+}
+
+if(keyPressed == '13'){
+    addUser();
+    console.log("Enter key pressed to log in");
+}
